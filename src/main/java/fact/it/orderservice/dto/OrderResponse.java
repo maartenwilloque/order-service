eaf9d892053ae3cd0fdae0882f4bc279484c4df9
MaartenWilloqué <r0237151@student.thomasmore.be>
2023-10-06 21:10:28 +0200

    init

---
+++ src/main/java/fact/it/orderservice/dto/OrderResponse.java
@@ -0,0 +1,15 @@
+package fact.it.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponse {
+    private String orderNumber;
+    private List<OrderLineItemDto> orderLineItemsList;
+}
